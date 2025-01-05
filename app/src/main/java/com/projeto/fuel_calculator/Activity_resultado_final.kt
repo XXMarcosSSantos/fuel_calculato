3f9ee46379f918d33f81cb8f8cdfb52624ef44ff
marco <marcosxsantosbkj@gmail.com>
2025-01-05 12:58:57 -0300

    Resultado final calculadora de combustivel

--- app/src/main/java/com/projeto/fuel_calculator/Activity_resultado_final.kt
+++ app/src/main/java/com/projeto/fuel_calculator/Activity_resultado_final.kt
@@ -1,6 +1,7 @@
 package com.projeto.fuel_calculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,22 @@ class Activity_resultado_final : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_resultado_final)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val preco = intent.getFloatExtra(KEY_PRECO,0f)
+        val consumo = intent.getFloatExtra(KEY_CONSUMO,0f)
+        val destino = intent.getFloatExtra(KEY_DESTINO,0f)
+        val resultadoFinal = (destino/consumo) * preco
+
+        val tvpreco = findViewById<TextView>(R.id.tv_preco_result)
+        val tvconsumo = findViewById<TextView>(R.id.tv_consumo_result)
+        val tvdestino = findViewById<TextView>(R.id.tv_km_result)
+        val tvresultado = findViewById<TextView>(R.id.tv_resultado)
+
+
+        tvpreco.text = preco.toString()
+        tvresultado.text = resultadoFinal.toString()
+        tvconsumo.text = consumo.toString()
+        tvdestino.text = destino.toString()
+
     }
 }
